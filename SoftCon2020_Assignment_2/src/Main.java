2ba80175ac8cf53ebcf7e79515e8ec6aa953c285
Nicolas Huber <56343633+HuberNicolas@users.noreply.github.com>
2020-10-07 15:38:31 +0200

    Created Ship Classes

--- SoftCon2020_Assignment_2/src/Main.java
+++ SoftCon2020_Assignment_2/src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+
+    // PRINT "Welcome to Battleship!!"
+
+    // INPUT
+
+    // Enter positions
+    // 1x Carrier (6)
+    // 2x Battleship (4)
+    // 3x Submarine (3)
+    // 4x Patrol Boat (2)
+
+
+    // OUTPUT
+
+    // PRINT "Your board is:"
+    // printBoard(gameBoard Board)
+
+
+
+
 }
--- SoftCon2020_Assignment_2/src/Ship.java
+++ SoftCon2020_Assignment_2/src/Ship.java
@@ -1,2 +1,15 @@
 public class Ship {
+    public String name; // eg. "Carrier"
+    public int length; // eg. "4"
+    public String shortName; // eg. "C"
+
+
+    private Ship(String n, int l, String sN) {
+        name = n;
+        length = l;
+        shortName = sN;
+    }
+
+
 }
+
