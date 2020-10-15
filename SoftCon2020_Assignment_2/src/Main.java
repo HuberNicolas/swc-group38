efa1e602e8ee0c982e13c026509d8f33bca8a33b
Nicolas Huber <56343633+HuberNicolas@users.noreply.github.com>
2020-10-15 21:36:52 +0200

    Finished V2
    try catch
    formatting
    commenting

--- SoftCon2020_Assignment_2/src/Main.java
+++ SoftCon2020_Assignment_2/src/Main.java
@@ -1,67 +1,93 @@
+/**
+ * Description:             A 2dim Version of the BATTLESHIP GAME
+ * @Authors:                Robin Wassink, Louis Huber and Nicolas Huber
+ */
+
 import java.util.List;
 import java.util.ArrayList;
 
-// This is the version without exceptions
 public class Main {
     public static void main(String [] args) {
-
         System.out.println("Welcome to Battleship!!");
-
         // Init gameBoard Board
         GameBoard Board = new GameBoard();
-        GameBoard.printBoard(Board); // Working
+        //GameBoard.printBoard(Board); // DEBUG
 
         // Generating all Ships and store them in shipList
         List<Ship> shipList = new ArrayList<Ship>(4);
-        int shipListIterator = 0;
+        int shipListIterator = 0; // number of all ships, regardless of type
+        // Fill shipList / board with 4 types of different ships
         for(int i = 0; i < 4; i++) {
             switch (i) {
-                case 0:
+                case 0: // CARRIER
                     Ship Carrier = new Ship("Carrier",6,"C");
                     shipList.add(Carrier);
-                    System.out.print(" Please enter the position of your Carrier: ");
-                    Move.makeMove(Board, shipList.get(shipListIterator));
-                    shipListIterator++;
+                    while (true) {
+                        try { // try to place a ship
+                            System.out.print(" Please enter the position of your Carrier: ");
+                            Move.makeMove(Board, shipList.get(shipListIterator));
+                            shipListIterator++;
+                            break;
+                        } catch (Exception e) {
+                            System.err.println("You cannot place your ship here! " + e.getMessage());
+                        }
+                    }
                     break;
-                case 1:
+                case 1: // BATTLESHIP
                     for(int j = 1; j < 3; j++) {
                         Ship Battleship = new Ship("Battleship",4,"B");
                         shipList.add(Battleship);
-                        System.out.print(" Please enter the position of your Battleship "+ j +": ");
-                        Move.makeMove(Board, shipList.get(shipListIterator));
-                        shipListIterator++;
+                        while (true) {
+                            try { // try to place a ship
+                                System.out.print(" Please enter the position of your Battleship " + j + ": ");
+                                Move.makeMove(Board, shipList.get(shipListIterator));
+                                shipListIterator++;
+                                break;
+                            } catch (Exception e) {
+                                System.err.println("You cannot place your ship here! " + e.getMessage());
+                            }
+                        }
                     }
                     break;
-                case 2:
+                case 2: // SUBMARINE
                     for(int j = 1; j < 4; j++) {
                         Ship Submarine = new Ship("Submarine",3,"S");
                         shipList.add(Submarine);
-                        System.out.print(" Please enter the position of your Submarine "+ j +": ");
-                        Move.makeMove(Board, shipList.get(shipListIterator));
-                        shipListIterator++;
+                        while (true) {
+                            try { // try to place a ship
+                                System.out.print(" Please enter the position of your Submarine "+ j +": ");
+                                Move.makeMove(Board, shipList.get(shipListIterator));
+                                shipListIterator++;
+                                break;
+                            } catch (Exception e) {
+                                System.err.println("You cannot place your ship here! " + e.getMessage());
+                            }
+                        }
                     }
                     break;
-                case 3:
+                case 3: // PATROL BOAT
                     for(int j = 1; j < 5; j++) {
                         Ship PatrolBoat = new Ship("Patrol boat",2,"P");
                         shipList.add(PatrolBoat);
-                        System.out.print(" Please enter the position of your Patrol boat "+ j +": ");
-                        Move.makeMove(Board, shipList.get(shipListIterator));
-                        shipListIterator++;
+                        while(true) {
+                            try { // try to place a ship
+                                System.out.print(" Please enter the position of your Patrol boat "+ j +": ");
+                                Move.makeMove(Board, shipList.get(shipListIterator));
+                                shipListIterator++;
+                                break;
+                            } catch (Exception e) {
+                                System.err.println("You cannot place your ship here! " + e.getMessage());
+                            }
+                        }
                     }
                     break;
             }
         }
-
-        //System.lineSeparator(); // ???
-
+        /*DEBUG
         // Test to access all the attributes of the shipList // works
-        //System.out.println("###########################################");
         for(Ship ship : shipList) {
-            //System.out.println(ship.getName() + " has length " + ship.length);
-        }
-
-        System.out.println("Your board is: ");
+        * //System.out.println(ship.getName() + " has length " + ship.length);
+        }*/
         GameBoard.printBoard(Board);
     }
 }
--- SoftCon2020_Assignment_2/src/Utils.java
+++ SoftCon2020_Assignment_2/src/Utils.java
@@ -1,4 +1,15 @@
+/**
+ * CLASS Utils
+ * Class for utility functions
+ */
 public class Utils {
+    /**
+     *
+     * @param cUInput:                      string userinput, may needs to be rejected
+     * @return                              Int Array[] with 4 coordinates/entries: [x_1,y_1,x_2,y_2]
+     * Discription:                         This method translates the checks the userinput and
+     *                                      converts it into the format [x_1,y_1,x_2,y_2]
+     */
     static Integer[] cUInputToGameCord(String cUInput) {
         //tansform Input "A1 B1" to [0, 1, 1, 1]
         Integer[] Gamecord = new Integer[4];
@@ -12,14 +23,26 @@ public class Utils {
         Gamecord[3] = cUInput.charAt(4) - 48;
         return Gamecord;
     }
+
+    /**
+     *
+     * @param Gamecoord int Arr[]           the entered coordinates, that need to be checked.
+     *                                      (4 entries : [x_1,y_1,x_2,y_2])
+     * @param sN String "sN":               length of the to be placed ship
+     * @param len int "len":                shortname for the to be placed ship
+     * @return
+     * Discription:                         This method checks, if the user made a valid move:
+     *                                      - ship is not diagonal
+     *                                      - ship is not out of field
+     *                                      - ship has the correct length
+     */
     static boolean validMove(Integer [] Gamecoord, String sN, Integer len) {
-        //function is not tested
         //check if horizontal or vertical -> no diagonal possible
     if (Gamecoord[0]!=Gamecoord[2] && Gamecoord[1] != Gamecoord[3]) {
         throw new IllegalArgumentException("No diagonal ships allowed");
          }
         //check if out of field
-    if (Gamecoord[0]<0 || Gamecoord[1]<0 ||Gamecoord[2]<0 ||Gamecoord[3]<0 ||Gamecoord[0]>10 ||Gamecoord[1]>10 ||Gamecoord[2]>10 ||Gamecoord[3]>10 ){
+    if (Gamecoord[0]<0 || Gamecoord[1]<0 || Gamecoord[2]<0 || Gamecoord[3]<0 || Gamecoord[0]>10 || Gamecoord[1]>10 || Gamecoord[2]>10 || Gamecoord[3]>10 ){
         throw new IllegalArgumentException("Out of Gameboard");}
         //check if length of ship is valid
     if (Math.abs(Gamecoord[0]-Gamecoord[2]) != (len-1) && Math.abs(Gamecoord[1]-Gamecoord[3]) != (len-1)) {
