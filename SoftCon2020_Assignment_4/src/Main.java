142f67d338e3484f4298c2b5cb7f9c545edd47b8
Nicolas Huber <56343633+HuberNicolas@users.noreply.github.com>
2020-11-04 15:22:38 +0100

    Initial Commit

---
+++ SoftCon2020_Assignment_4/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Description:             A 2dim Version of the BATTLESHIP GAME
+ * @Authors:                Robin Wassink, Louis Huber and Nicolas Huber
+ *
+ */
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String [] args) {
+        System.out.println("Hello Word");
+    }
+}
