61586c70a01cf7127dcdaf3543b3cf0996aef772
rwassi <robin.wassink@uzh.ch>
2020-11-23 13:43:05 +0100

    added tests to task 2

--- SoftCon2020_Assignment_5/AirportShuttleService/test/MainTest.java
+++ SoftCon2020_Assignment_5/AirportShuttleService/test/MainTest.java
@@ -2,6 +2,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
@@ -11,8 +14,52 @@ class MainTest {
     }
 
     @Test
-    void test() {
+    public void MicroCarTest(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
 
+        Customer Sandro = new Customer("12.03.2020", new MicroCar());
+        Sandro.ride();
+
+        String expectedOutput  = "Micro Car; 1 small bag; normal speed; 12 CHF/h\n";
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+    @Test
+    public void BusTest(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Customer Marco = new Customer("12.04,2020", new Bus());
+        Marco.ride();
+
+        String expectedOutput  = "Bus; unlimited bags; slow speed; 5 CHF\n";
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+    @Test
+    public void SupercarTest(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Customer Felix = new Customer("12.05,2020", new Supercar());
+        Felix.ride();
+
+        String expectedOutput  = "Supercar; 1 small and 1 large bag; fast speed; 30 CHF/h\n";
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+    @Test
+    public void FamilyCarTest(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Customer George = new Customer("12.06,2020", new FamilyCar());
+        George.ride();
+
+        String expectedOutput  = "Family Car; 2 small and 2 large bags; normal speed; 15 CHF/h\n";
+
+        assertEquals(expectedOutput, outContent.toString());
     }
 
     @After
