2728b98a6a65652607fec2e44efe80cf330a66ff
Bolzmann\Louis <Louisp.huber@gmx.ch>
2020-12-02 15:57:20 +0100

    Update Utils.java
    static int generateID() created

--- SoftCon2020_Assignment_6/CompanySystem/src/Utils.java
+++ SoftCon2020_Assignment_6/CompanySystem/src/Utils.java
@@ -1,2 +1,20 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
 public class Utils {
+    static ArrayList<Integer> IDList = new ArrayList<Integer>();
+
+    /**
+     *
+     * @return: ID which is 8 digits and unique
+     */
+    static int generateID(){
+        // generates ID
+        int ID = ThreadLocalRandom.current().nextInt(10000000, 100000000);
+        while (IDList.contains(ID)){
+            ID = ThreadLocalRandom.current().nextInt(10000000, 100000000);
+        }
+        IDList.add(ID);
+        return ID;
+    }
 }
